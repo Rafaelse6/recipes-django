426ee170ec0986ccc1fb1727079211dfcdaffadf
Rafael Santos <rafa.05125@gmail.com>
2024-02-22 20:20:36 -0300

    Temp templates example

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html')
+   return render(request, 'recipes/home.html', context={
+       'name': 'Rafael Santos',
+   })
 
 def contact(request):
-    return HttpResponse('contact')
+    return render(request, 'delete/temp.html')
 
 def about(request):
     return HttpResponse('about')
