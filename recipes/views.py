8563288bed03971c344f51448be0c9951830d754
Rafael Santos <rafa.05125@gmail.com>
2024-02-25 19:45:03 -0300

    Deleting contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
    return render(request, 'recipes/home.html', context={
        'name': 'Rafael Santos',
    })
-
-def contact(request):
-    return render(request, 'recipes/contact.html')
-
-def about(request):
-    return HttpResponse('about')
