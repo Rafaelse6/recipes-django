babe30bdd313cf035ffd80c4b4cf6913e0ff714d
Rafael Santos <rafa.05125@gmail.com>
2024-03-10 11:15:05 -0300

    Using get_list_or_404 shortcut

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from .utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
@@ -8,10 +7,6 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
-
-    if not recipes:
-        raise Http404('Not found!')
-
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
         'title': f'{recipes.first().category.name} - Category | '
@@ -19,12 +14,15 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
