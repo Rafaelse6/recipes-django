439f5e18448fd1594d587f572392676fe962e1b4
Rafael Santos <rafa.05125@gmail.com>
2024-03-13 08:57:26 -0300

    Pytest and Pytest-Django initial configs

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase  # ignore
 
 # Create your tests here.
+
+
+class RecipeURLsTest(TestCase):
+    def test_if_pytest_is_ok(self):
+        assert 1 == 1, 'The result is 1'
