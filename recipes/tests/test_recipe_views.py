25765f6dd46f36fd51b2081b8429cd817ccd649e
Rafael Santos <rafa.05125@gmail.com>
2024-03-30 23:56:11 -0300

    Extracting methods

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,8 +4,6 @@ from .test_recipe_base import RecipeTestBase
 
 
 class RecipeViewsTest(RecipeTestBase):
-    def tearDown(self) -> None:
-        return super().tearDown()
 
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
@@ -28,13 +26,15 @@ class RecipeViewsTest(RecipeTestBase):
 
     def test_recipe_home_template_loads_recipes(self):
 
+        # Need a recipe for this test
+        self.make_recipe()
+
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
 
+        # Check if a recipe exists
         self.assertIn('Recipe title', content)
-        self.assertIn('10 Minutos', content)
-        self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_view_function_is_correct(self):
