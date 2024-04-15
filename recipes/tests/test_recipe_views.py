ba5058fed74431c089bfef873e192da6c945dffd
Rafael Santos <rafa.05125@gmail.com>
2024-04-14 23:59:18 -0300

    Fix: recipe.category.id to recipe.id

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -127,7 +127,7 @@ class RecipeViewsTest(RecipeTestBase):
             reverse(
                 'recipes:recipe',
                 kwargs={
-                    'id': recipe.category.id
+                    'id': recipe.id
                 }
             )
         )
